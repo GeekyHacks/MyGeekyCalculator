ba5481cbb65094be09b26482ea92589cd65dab68
GeekyHacks <geekyhacks22@gmail.com>
2023-08-23 16:01:32 +0300

    Apply required changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import QuoteApi from './components/NinjaAPI';
+import Contact from './components/Contact';
+import Calculator from './components/Calculator';
 import Footer from './components/Footer';
 import './Styles/Css/App.css';
-import FullWebsite from './components/FullWebsite';
 
 function App() {
   return (
-    <div className="App">
-      <FullWebsite />
+    <div className="fullWebsite">
+      <div className="App">
+        <nav className="navBar">
+          <h2>Geeky Calculator</h2>
+          <div>
+            <Link className="link" to="/calculator">
+              Calculator
+            </Link>
+            <span>|</span>
+            <Link className="link" to="/quotes">
+              Quotes
+            </Link>
+
+            <span>|</span>
+            <Link className="link" to="/contact">
+              Contact
+            </Link>
+          </div>
+        </nav>
+
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quotes" element={<QuoteApi />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </div>
+
       <Footer />
     </div>
   );
